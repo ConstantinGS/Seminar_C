c771b5922c5aa3104033e02f2bc37d8fb85d0acd
Constantin <grishanoff.konstantin@yandex.ru>
2022-02-28 08:24:34 +0500

    Complete ex8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while (i<=number)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
+
+
+
+
+
+
