d0729c3710c15baff6352300cac3593cd9b56789
Constantin <grishanoff.konstantin@yandex.ru>
2022-02-28 08:11:24 +0500

    Complete ex 6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number%2==0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else if (number%2!=0)
+            {
+                Console.WriteLine("Число нечетное"); 
+            }
+
+
+
+
+
+
+        }
+     }
+}
